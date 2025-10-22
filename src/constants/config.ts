914cf3ab35a9e9458221158a9affa99148affeec
xDanial5959 <muhammaddanial5959@gmail.com>
2025-10-22 22:37:52 +0500

    first commit

---
+++ src/constants/config.ts
@@ -0,0 +1,90 @@
+type TSection = {
+  p: string;
+  h2: string;
+  content?: string;
+};
+
+type TConfig = {
+  html: {
+    title: string;
+    fullName: string;
+    email: string;
+  };
+  hero: {
+    name: string;
+    p: string[];
+  };
+  contact: {
+    form: {
+      name: {
+        span: string;
+        placeholder: string;
+      };
+      email: {
+        span: string;
+        placeholder: string;
+      };
+      message: {
+        span: string;
+        placeholder: string;
+      };
+    };
+  } & TSection;
+  sections: {
+    about: Required<TSection>;
+    experience: TSection;
+    feedbacks: TSection;
+    works: Required<TSection>;
+  };
+};
+
+export const config: TConfig = {
+  html: {
+    title: 'Danial Akbar',
+    fullName: 'Danial Akbar',
+    email: 'muhammaddanial5959@gmial.com',
+  },
+  hero: {
+    name: 'Danial Akbar',
+    p: ['I’m a Web Developer focused on creating modern, responsive, and user-friendly web applications.'],
+  },
+  contact: {
+    p: 'Get in touch',
+    h2: 'Contact.',
+    form: {
+      name: {
+        span: 'Your Name',
+        placeholder: "What's your name?",
+      },
+      email: { span: 'Your Email', placeholder: "What's your email?" },
+      message: {
+        span: 'Your Message',
+        placeholder: 'What do you want to say?',
+      },
+    },
+  },
+  sections: {
+    about: {
+      p: 'Introduction',
+      h2: 'Overview.',
+      content: `Hi, I’m Dani — a passionate web developer who loves turning ideas into clean, functional, and visually appealing digital experiences. I enjoy building responsive, user-friendly interfaces and crafting efficient solutions using modern web technologies. Whether it’s front-end polish or back-end logic, I aim to create products that not only work smoothly but feel great to use.`,
+    },
+    experience: {
+      p: 'What I have done so far',
+      h2: 'Work Experience.',
+    },
+    feedbacks: {
+      p: 'What others say',
+      h2: 'Testimonials.',
+    },
+    works: {
+      p: 'My work',
+      h2: 'Projects.',
+      content: `Following projects showcases my skills and experience through
+    real-world examples of my work. Each project is briefly described with
+    links to code repositories and live demos in it. It reflects my
+    ability to solve complex problems, work with different technologies,
+    and manage projects effectively.`,
+    },
+  },
+};
